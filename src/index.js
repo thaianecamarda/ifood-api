5b40abd6614bb8d6cab9bb1440934eb46d7f8115
thaianecamarda <thaianecamarda@gmail.com>
2022-06-29 22:30:18 -0300

    adicao do post de restaurantes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
+const restaurantes = []
+
 app.get('/', (req, res) => {
     res.send('Ifood API')
+})
+
+app.post('/restaurante', (req, res) => {
+    const { nome, endereco } = req.body
+
+    const novoRestaurante = {
+        id: restaurante.length + 1,
+        nome: nome,
+        endereco: endereco,
+        cardapio: []
+    }
+    restaurantes.push(novoRestaurante)
+
+    res.status(201).json(novoRestaurante)
 })
 
 app.listen(3333, () => {
